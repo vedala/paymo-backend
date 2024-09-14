a33dda930a112b7ddd24d53dd37d7733540c8772
Kalyan Vedala <svedala23@gmail.com>
2024-09-14 10:58:28 -0700

    Fetch data from table in getBanks handler

--- handlers.js
+++ handlers.js
@@ -1,9 +1,17 @@
+import getKnexObj from "./getKnexObj.js";
+
+const knex = getKnexObj();
+
 const getWelcome = async (req, res) => {
   res.send("Welcome to Paymo!");
 }
 
 const getBanks = async (req, res) => {
-  res.send(JSON.stringify([{id: 100, name: "Bank 100"}, {id: 200, name: "Bank 200"}]));
+  const rows = await knex(process.env.BANKS_TABLE_NAME).select('id', 'name')
+    .orderBy('id')
+    .catch((err) => { console.error(err); throw err; });
+
+  res.send(rows);
 }
 
 export { getWelcome, getBanks };

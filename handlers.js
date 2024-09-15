604ba72ecec6bc4734c0a8d4a3c59fdd4870bf4a
Kalyan Vedala <svedala23@gmail.com>
2024-09-15 09:20:10 -0700

    Add exchange public token handler

--- handlers.js
+++ handlers.js
@@ -1,6 +1,20 @@
 import getKnexObj from "./getKnexObj.js";
+import { Configuration, PlaidApi } from "plaid";
 
 const knex = getKnexObj();
+
+const plaidConfig = new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET,
+      "Plaid-Version": "2020-09-14",
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(config);
 
 const getWelcome = async (req, res) => {
   res.send("Welcome to Paymo!");
@@ -16,6 +30,15 @@ const getBanks = async (req, res) => {
 
 const createLinkToken = async (req, res) => {
   return "{}";
-};
+}
+
+const exchangePublicToken = async (req, res) => {
+  const exchangeResponse = await plaidClient.exchangePublicToken({
+    public_token: req.body.public_token,
+  });
+
+  req.session.access_token = exchangeResponse.data.access_token;
+  res.json(true);
+}
 
 export { getWelcome, getBanks, createLinkToken };

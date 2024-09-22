a222266d278e5bf45d9cad319af67d5442f9fa1e
Kalyan Vedala <svedala23@gmail.com>
2024-09-22 09:20:31 -0700

    Create and save management access token at application startup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import  { Router } from "express";
 import routes from "./routes.js";
 import { getWelcome, getBanks, getRecipients, getUserByEmail, createLinkToken, exchangePublicToken } from "./handlers.js";
 import fetchManagementApiAccessToken from "./fetchManagementApiAccessToken.js";
+import saveManagementToken from "./saveManagementToken.js";
 
 const app = express();
 
@@ -18,7 +19,8 @@ const port = 3000;
 app.use("/", router);
 routes(router, { getWelcome, getBanks, getRecipients, getUserByEmail, createLinkToken, exchangePublicToken });
 
-await fetchManagementApiAccessToken();
+const managementApiAccessToken = await fetchManagementApiAccessToken();
+await saveManagementToken(managementApiAccessToken);
 
 app.listen(port, () => {
   console.log(`Paymo app listening on port ${port}`);
